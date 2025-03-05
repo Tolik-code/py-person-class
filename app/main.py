ad8aa1bf27a88e14afa1916681839149a7344f8d
Tolik-code <72889479+Tolik-code@users.noreply.github.com>
2025-03-05 21:50:30 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name, self.age = name, age
+        self.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list[dict]) -> list[dict]:
+    for person_data in people:
+        Person(name=person_data["name"], age=person_data["age"])
+    for person_data in people:
+        if "wife" in person_data and person_data["wife"]:
+            Person.people[person_data["name"]].wife = (
+                Person.people[person_data["wife"]])
+            Person.people[person_data["wife"]].husband = Person.people[
+                person_data["name"]
+            ]
+    return list(Person.people.values())
